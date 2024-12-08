accd8f208c9b288daa0afd0f3c97c776f00af248
Your Name <you@example.com>
2024-12-08 18:56:15 +0100

    nft-project

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,28 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import Home from "./page";
+
+import './styles/globals.css'
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+import { ThemeProvider } from "next-themes";
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-       
-      >
-       <Home />
+      <body>
+      <ThemeProvider attribute="class">
+        <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+          {children}
+          <Footer />
+        </div>
+        
+        </ThemeProvider>
       </body>
     </html>
   );
